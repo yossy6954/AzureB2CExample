8d26b492406810451f3a7cccad23a09114642ed2
yossy6954 <yossy6954@gmail.com>
2018-04-20 12:57:44 +0900

    First commit

---
+++ AzureB2CExample/Controllers/DebugController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace AzureB2CExample.Controllers {
+    [Route("api/[controller]")]
+    public class DebugController : Controller {
+        // GET api/Debug/me
+        [Authorize]
+        [HttpGet("me")]
+        public Dictionary<string, string> Me() {
+            return (User.Identity as ClaimsIdentity).Claims
+                .ToDictionary(c => c.Type, c => c.Value);
+        }
+    }
+}
